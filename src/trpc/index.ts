bcd53e5c600d8c71c0787f071aa77a8c8e443b9d
Adarsh Mehta <mehtaadarsh01@gmail.com>
2024-02-05 08:40:41 +0530

    dashboard UI setup

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,21 +1,60 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
-import {publicProcedure, router} from './trpc'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import {privateProcedure, publicProcedure, router} from './trpc'
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
+import { useStyleRegistry } from 'styled-jsx';
 
 export const appRouter = router({
-    authCallback: publicProcedure.query(()=>{
+    authCallback: publicProcedure.query( async ()=>{
         const {getUser} = getKindeServerSession()
-        const user = getUser()
+        const user = await getUser()
 
-        // if(!user.id || !user.email)
-        if(!user)
+        if(!user?.id || !user.email)
+        
         {
             throw new TRPCError({code: 'UNAUTHORIZED'})
+        }
+
+        //check if user is in the database
+        const dbUser = await db.user.findFirst({
+            where: {
+                id: user?.id
+                
+            }
+        })
+
+        if(!dbUser) {
+            await db.user.create({
+                data : {
+                    id:user.id,
+                   
+                    email:user.email
+                    
+                }
+            })
         }
 
 
         return {success: true}
     }),
+
+    getUserFiles: privateProcedure.query( async ({ctx})=>{
+        const {userId} = ctx
+
+    //     return await db.file.findMany({
+    //         where: {
+    //             userId
+    //         },
+    //     })
+        
+    // })
+        return await db.user.findMany({
+            where: {
+                id:userId
+            },
+        })
+        
+    })
 });
 
 export type AppRouter = typeof appRouter;
