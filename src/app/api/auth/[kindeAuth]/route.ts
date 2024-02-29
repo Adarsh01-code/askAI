062aab52bff4f944d9503fe88a4365720ba8e9d0
Adarsh Mehta <mehtaadarsh01@gmail.com>
2024-03-01 03:10:13 +0530

    Resolving vercel deployment errors

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,24 +1,14 @@
-// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-// import { NextRequest, NextResponse } from "next/server";
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(request: NextRequest,{params}:any) {
-//     const endpoint = params.kindeAuth;
-//     return handleAuth(request,endpoint);
-// }
-
-
-
-
-
-
-import { NextApiRequest, NextApiResponse } from 'next';
-
-function handleAuthRoute(req: NextApiRequest, res: NextApiResponse) {
-
-  res.status(200).json({ message: 'Auth route response' });
+export async function GET(request: NextRequest,{params}:any) {
+    const endpoint = params.kindeAuth;
+    return handleAuth(request,endpoint);
 }
 
 
-export default handleAuthRoute;
+
+
+
 
 
