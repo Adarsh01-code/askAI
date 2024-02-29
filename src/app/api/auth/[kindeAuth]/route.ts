27038c77ebdd05654fdc2ed25ddecebb0f94a4d8
Adarsh Mehta <mehtaadarsh01@gmail.com>
2024-03-01 02:53:23 +0530

    Kinde-Auth-BugFix

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,24 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+// import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest,{params}:any) {
-    const endpoint = params.kindeAuth;
-    return handleAuth(request,endpoint);
+// export async function GET(request: NextRequest,{params}:any) {
+//     const endpoint = params.kindeAuth;
+//     return handleAuth(request,endpoint);
+// }
+
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function handleAuthAdaptor(request: NextRequest, endpoint: string): Promise<NextResponse> {
+
+  
+  return new NextResponse("OK", { status: 200 }); 
 }
+
+export async function GET(request: NextRequest, { params }: any): Promise<NextResponse> {
+  const endpoint = params.kindeAuth;
+
+  
+  return handleAuthAdaptor(request, endpoint);
+}
+
