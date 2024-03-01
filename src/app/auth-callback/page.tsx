3acccabb3b8b279e57c83aecd7e8483d7365d9fe
Adarsh Mehta <mehtaadarsh01@gmail.com>
2024-03-01 05:41:50 +0530

    MOCK

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -4,7 +4,7 @@ import { useRouter, useSearchParams } from "next/navigation"
 import { trpc } from "../_trpc/client"
 import { Loader2 } from "lucide-react"
 import { TRPCClientError } from '@trpc/client';
-
+import { Suspense } from "react";
 
 
 type AuthCallbackResponse = {
@@ -36,13 +36,15 @@ const Page =  () => {
     })
 
     return (
-        <div className="w-full mt-24 flex justify-center">
+        <Suspense>
+            <div className="w-full mt-24 flex justify-center">
             <div className="flex flex-col items-center gap-2">
                 <Loader2 className="h-8 w-8 animate-spin text-zinc-800"/>
                 <h3 className="font-semibold text-xl">Setting up your account ...</h3>
                 <p>You will be redirected automatically</p>
             </div>
         </div>
+        </Suspense>
         
     )
     
