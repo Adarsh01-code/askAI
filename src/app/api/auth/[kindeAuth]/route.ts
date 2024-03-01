8bd0444ed147185f6bc6de2b915cc32ef00c60c0
Adarsh Mehta <mehtaadarsh01@gmail.com>
2024-03-01 06:11:09 +0530

    pre-deploy commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,19 +1,17 @@
 
 
-// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-// import { NextRequest, NextResponse } from "next/server";
-
-
-
-// export async function GET(request: NextRequest,{params}:any) {
-//     const endpoint = params.kindeAuth;
-//     return handleAuth(request,endpoint);
-// }
-
-
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = handleAuth();
+
+
+export async function GET(request:NextRequest,{params}:any) {
+    const endpoint = params.kindeAuth;
+    return handleAuth(request,endpoint);
+}
+
+
+
 
 
 
