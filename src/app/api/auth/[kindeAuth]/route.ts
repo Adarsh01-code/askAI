2a77f465f7c514d1f9d7829f5bd1f89bb161e33f
Adarsh Mehta <mehtaadarsh01@gmail.com>
2024-03-01 21:48:27 +0530

    mock

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,16 +1,16 @@
 
 
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
-
-export async function GET(request:NextRequest,{params}:any) {
-    const endpoint = params.kindeAuth;
-    return handleAuth(request,endpoint);
-}
-
 // import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+// import { NextRequest } from "next/server";
 
-// export const GET = handleAuth();
+// export async function GET(request:NextRequest,{params}:any) {
+//     const endpoint = params.kindeAuth;
+//     return handleAuth(request,endpoint);
+// }
+
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+
+export const GET = handleAuth();
 
 
 
